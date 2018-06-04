9a86a1df74c2607bc26d1b2b1665af6cbf1a7de3
Unknown <rowanvanrhijn@gmail.com>
2018-06-04 10:27:04 -0400

    04/06/2018 Incomplete movement file

--- core/src/main/game/Main.java
+++ core/src/main/game/Main.java
@@ -2,6 +2,7 @@ package main.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,7 +10,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture Idle1, Idle2;
-	int nCount = 0;
+	Texture PunchStraight, PunchHook, PunchUp;
+	Texture KickStraight1, KickStraight2, KickJump1, KickJump2, KickLow1, KickLow2;
+	int nFrame = 0, nIdle = 0, nJump = 0, nX = 250, nY = 50; //Hero's variables
 	
 	@Override
 	public void create () {
@@ -23,9 +26,38 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		hero (nCount);
-		if (nCount == 50) nCount = 0;
-		nCount++;
+
+
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && nX <= 540) {
+			nX += 3;
+			nFrame = 2;
+		}
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && nX >= 0) {
+			nX -= 3;
+			nFrame = 3;
+		}
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && nJump == 0) {
+        	nJump = 49;
+        	nFrame = 4;
+		}
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && nJump == 0) {
+			if (nFrame == 0 || nFrame == 2) nFrame = 5;
+			if (nFrame == 1 || nFrame == 3) nFrame = 6;
+		}
+
+
+        if (nJump > 29) {
+        	nY += 5;
+		}
+		else if (nJump <= 20 && nJump > 0) {
+			nY -= 5;
+		}
+		hero (nIdle ,nX, nY);
+		//Whole game's gonna run off this count, I may tweak numbers a bit but this is important I think
+        //Maybe just tyrone idk
+		if (nJump > 0) nJump--;
+		if (nIdle == 50) nIdle = 0;
+		nIdle++;
 		batch.end();
 	}
 	
@@ -36,8 +68,8 @@ public class Main extends ApplicationAdapter {
 		Idle2.dispose();
 	}
 
-	public void hero (int nCount) {
-        if (nCount <= 24) batch.draw(Idle1, 250, 50, 100, 250);
-        if (nCount > 24) batch.draw(Idle2, 250, 50, 100, 250);
+	public void hero (int nCount, int nX, int nY) {
+        if (nCount <= 24) batch.draw(Idle1, nX, nY, 100, 250);
+        if (nCount > 24) batch.draw(Idle2, nX, nY, 100, 250);
 	}
 }
